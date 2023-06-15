0dd585e50bc188f92486ed43d372d552e6c2fe20
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-06-15 18:24:13 +0300

    config of swagger installing Config Module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Hotels API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(4000);
 }
-bootstrap();
+bootstrap().then();
